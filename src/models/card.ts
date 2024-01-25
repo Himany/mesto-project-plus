6b19c67434d6eac0f6e427a047d8679a0be73f60
Himany <Himaaan@yandex.ru>
2024-01-26 00:22:33 +0300

    add link validator in card model

--- src/models/card.ts
+++ src/models/card.ts
@@ -1,4 +1,5 @@
 import { model, Schema } from 'mongoose';
+import { regexUrl } from '../utils/constants';
 
 export interface ICard {
   name: string;
@@ -18,6 +19,10 @@ const cardSchema = new Schema<ICard>({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator: (v: string) => regexUrl.test(v),
+      message: 'Некорректный URL (Изображение)',
+    },
   },
   owner: {
     type: Schema.Types.ObjectId,
