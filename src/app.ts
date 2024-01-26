6c59c0e01a7b1cea39f5e37acece9b36449e8cf9
Himany <Himaaan@yandex.ru>
2024-01-26 07:37:19 +0300

    add mongo db address in ENV

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,13 @@ import router from './routes/router';
 import error from './middlewares/errors';
 import { requestLogger, errorLogger } from './middlewares/logger';
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGO_DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(MONGO_DB_ADDRESS);
 
 app.use(requestLogger);
 app.use(router);
